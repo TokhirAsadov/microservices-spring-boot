99e7b62924d9c920d6dce3b19de153284e28878f
Takhir <guvalakat1603@gmail.com>
2025-01-17 10:08:22 +0500

    OrderRepository is created

---
+++ OrderService/src/main/java/com/javatuz/OrderService/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.javatuz.OrderService.repository;
+
+import com.dailycodebuffer.OrderService.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> {
+}
