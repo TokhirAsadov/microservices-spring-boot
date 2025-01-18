9b2710e81ff6273d028047ab98e7f262d655a012
Takhir <guvalakat1603@gmail.com>
2025-01-18 10:25:34 +0500

    PaymentService is done, using @FeignClients

--- OrderService/src/main/java/com/javatuz/OrderService/external/client/PaymentService.java
+++ OrderService/src/main/java/com/javatuz/OrderService/external/client/PaymentService.java
@@ -3,13 +3,13 @@ package com.javatuz.OrderService.external.client;
 import com.javatuz.OrderService.exception.CustomException;
 import com.javatuz.OrderService.external.request.PaymentRequest;
 //import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-//import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 //@CircuitBreaker(name = "external", fallbackMethod = "fallback")
-//@FeignClient(name = "PAYMENT-SERVICE/payment")
+@FeignClient(name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
 
     @PostMapping
