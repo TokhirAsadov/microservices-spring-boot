61964d2e6f2723c2d6983fc8f14cdcf6ce37560f
Takhir <guvalakat1603@gmail.com>
2025-01-18 10:04:52 +0500

    TransactionDetailsRepository is created

---
+++ PaymentService/src/main/java/com/javatuz/PaymentService/repository/TransactionDetailsRepository.java
@@ -0,0 +1,11 @@
+package com.javatuz.PaymentService.repository;
+
+import com.javatuz.PaymentService.entity.TransactionDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionDetailsRepository extends JpaRepository<TransactionDetails, Long> {
+
+    TransactionDetails findByOrderId(long orderId);
+}
