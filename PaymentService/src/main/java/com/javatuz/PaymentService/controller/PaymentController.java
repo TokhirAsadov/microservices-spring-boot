c92a06b1cab42e30c51fda23ecf50ccef76dddd8
Takhir <guvalakat1603@gmail.com>
2025-01-18 10:05:22 +0500

    PaymentController is created

---
+++ PaymentService/src/main/java/com/javatuz/PaymentService/controller/PaymentController.java
@@ -0,0 +1,34 @@
+package com.javatuz.PaymentService.controller;
+
+import com.javatuz.PaymentService.model.PaymentRequest;
+import com.javatuz.PaymentService.model.PaymentResponse;
+import com.javatuz.PaymentService.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest) {
+        return new ResponseEntity<>(
+                paymentService.doPayment(paymentRequest),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<PaymentResponse> getPaymentDetailsByOrderId(@PathVariable String orderId) {
+        return new ResponseEntity<>(
+                paymentService.getPaymentDetailsByOrderId(orderId),
+                HttpStatus.OK
+        );
+    }
+
+}
