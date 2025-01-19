9aadbe5de420659dd03093d9538afe7ad246d693
Takhir <guvalakat1603@gmail.com>
2025-01-19 15:40:24 +0500

    @CircuitBreaker(name = "external", fallbackMethod = "fallback")

--- OrderService/src/main/java/com/javatuz/OrderService/external/client/PaymentService.java
+++ OrderService/src/main/java/com/javatuz/OrderService/external/client/PaymentService.java
@@ -2,13 +2,13 @@ package com.javatuz.OrderService.external.client;
 
 import com.javatuz.OrderService.exception.CustomException;
 import com.javatuz.OrderService.external.request.PaymentRequest;
-//import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-//@CircuitBreaker(name = "external", fallbackMethod = "fallback")
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
 
