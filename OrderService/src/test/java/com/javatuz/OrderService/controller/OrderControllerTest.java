e1459f50337d9ea66a4db5e782390c95bcc98bd5
Takhir <guvalakat1603@gmail.com>
2025-01-20 22:13:23 +0500

    OrderControllerTest

---
+++ OrderService/src/test/java/com/javatuz/OrderService/controller/OrderControllerTest.java
@@ -0,0 +1,15 @@
+package com.javatuz.OrderService.controller;
+
+import com.javatuz.OrderService.OrderServiceConfig;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest({"server.port=0"})
+@EnableConfigurationProperties
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = {OrderServiceConfig.class})
+public class OrderControllerTest {
+
+}
