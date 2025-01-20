e843a3f4ad45fcc1f3cc78683a83edcf06073a05
Takhir <guvalakat1603@gmail.com>
2025-01-20 15:05:25 +0500

    changing spring security 6.1

--- PaymentService/src/main/java/com/javatuz/PaymentService/security/WebSecurityConfig.java
+++ PaymentService/src/main/java/com/javatuz/PaymentService/security/WebSecurityConfig.java
@@ -2,9 +2,11 @@ package com.javatuz.PaymentService.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,13 +16,19 @@ public class WebSecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(authorizeRequests ->
+                .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/payment/**")
-                                .hasAuthority("SCOPE_internal")
-                                .anyRequest()
-                                .authenticated())
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+                                .requestMatchers("/payment/**").hasAuthority("SCOPE_internal")
+                                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
         return http.build();
     }
+
+//    @Bean
+//    public JwtDecoder jwtDecoder() {
+//        // Replace "your-jwk-set-uri" with your JWK Set URI from the authorization server
+//        return NimbusJwtDecoder.withJwkSetUri("your-jwk-set-uri").build();
+//    }
 }
